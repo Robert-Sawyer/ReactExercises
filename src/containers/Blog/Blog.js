41dfd9da45daf485e763efeff6e2d7ce3e63d59a
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-12-14 19:38:22 +0100

    Added redirection from root path to /posts.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Route, NavLink, Switch} from 'react-router-dom';
+import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
@@ -36,6 +36,7 @@ class Blog extends Component {
                     {/*dodajemy dwa Routy do switcha żeby react mógł wybrać jeden i nie mylił ścieżki*/}
                     <Route path="/new-post" component={NewPost}/>
                     <Route path="/posts" component={Posts}/>
+                    <Redirect from="/" to="/posts"/>
                 </Switch>
             </div>
         );
